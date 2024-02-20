ec2c61907fab9afe560a1194ba8143f26ea23855
Ricardo Frohlich da Silva <ricardosma@gmail.com>
2024-02-19 21:59:55 -0300

    Aula 1902

---
+++ AplicacaoWeb/Pages/Oi.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AplicacaoWeb.Pages
+{
+    public class OiModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
